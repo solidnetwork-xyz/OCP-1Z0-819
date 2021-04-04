df91e3142e31297a7f596ec4d5b6c6d9e2b59225
Jose Osuna <2281302+joseosuna-engineer@users.noreply.github.com>
2021-04-04 10:31:11 -0400

    adds console interaction

--- src/main/java/com/amazon/MainApplication.java
+++ src/main/java/com/amazon/MainApplication.java
@@ -4,13 +4,34 @@ import com.amazon.service.IDog;
 import com.amazon.dto.ResponseDto;
 import com.amazon.service.impl.DogServiceManager;
 import com.amazon.service.impl.DogServiceProcessor;
+import java.util.Scanner;
 
 //TODO: SpringBootApplication annotation
 public class MainApplication {
 
+    public static int getOption(){
+        //TODO: change console input to REST controller
+        System.out.print("Enter your option please: ");
+        Scanner consoleReader =  new Scanner(System.in);
+        return consoleReader.nextInt();
+    }
+
     public static void main(String[] args){
         //TODO: SpringApplication initialization
 
+        final int optionSelected = getOption();
+
+        switch(optionSelected){
+            case 1: multipleImplementationExample();
+            break;
+            default:
+                //TODO: change println to slf4j
+                System.out.println("out of options");
+        }
+
+    }
+
+    private static void multipleImplementationExample(){
         //TODO: change println to slf4j
         System.out.println("before calling dogService");
         //TODO: change new to @Autowired
