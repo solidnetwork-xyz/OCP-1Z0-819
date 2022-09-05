4543395fe3ed519b5d4e6cf9db33c212ec00cb50
Jose Osuna <2281302+joseosuna-engineer@users.noreply.github.com>
2022-09-05 10:12:48 -0300

    Updates Stream example.

--- src/main/java/com/prottonne/aaa/Example.java
+++ src/main/java/com/prottonne/aaa/Example.java
@@ -13,10 +13,13 @@
 
 package com.prottonne.aaa;
 
+import java.util.List;
+
 public class Example {
 
     public static void main(String[] args) {
-
+        List<String> s;
+        s.stream();
 
     }
 
